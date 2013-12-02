433e59bfce92e82bf622d8e25f521169d4895d8b
Kwabena Aning <kwabena.aning@gmail.com>
2013-12-02 00:33:01 +0000

    Using akka for this implementation

---
+++ src/main/scala/kaning/actors/Server.scala
@@ -0,0 +1,50 @@
+package kaning.actors
+
+import akka.actor.{Actor, Props, ActorSystem, ActorRef, PoisonPill}
+import collection.mutable.Set
+import kaning.messages._
+import kaning.actors._
+import com.typesafe.config.ConfigFactory
+
+object ChatServerApplication extends App{
+	println("Starting Akka Chat Server Actor") 
+	val system = ActorSystem("ChatServerApplication", ConfigFactory.load.getConfig("chatserver"))
+	val server = system.actorOf(Props[ChatServerActor], name = "chatserver")
+	server ! StartUp
+}
+
+class ChatServerActor extends Actor {
+
+	val connectedClients:Set[ActorRef] = Set()
+
+	def receive = {
+
+		case ChatMessage(x: String) => {
+			println(sender + ": " + x)
+			sender ! new ChatInfo("ACK")
+		}
+
+		case RegisterClientMessage(client: ActorRef) => {
+  			println("Client registering with server")
+  			this.connectedClients += client
+  			sender ! ChatInfo("REGISTERED")
+  		}
+
+  		case StartUp => {
+  			println("Received Start Server Signal")
+  			println(self)
+  		}
+
+  		case RegisteredClients => {
+  			sender ! RegisteredClientList(connectedClients)
+  		}
+        
+        case Unregister(client: ActorRef) => {
+            // remove client from registered client set and send poison pill
+            this.connectedClients -= client
+            sender ! PoisonPill
+        }
+
+		case _ => println("Server received message")
+	}
+}
---
+++ src/main/scala/kaning/messages/ChatMessages.scala
@@ -0,0 +1,18 @@
+package kaning.messages
+
+import akka.actor.{ActorRef}
+import kaning.actors._
+import collection.mutable.Set
+
+sealed trait  Message
+@serializable
+case class StartUp extends Message
+case class Register extends Message
+case class Disconnect extends Message
+case class ChatMessage(msg: String) extends Message
+case class ChatInfo(inf: String) extends Message
+case class RegisterClientMessage(client:ActorRef) extends Message
+case class Unregister(client: ActorRef) extends Message
+case class RegisteredClients extends Message
+case class RegisteredClientList(list: Set[ActorRef]) extends Message
+case class Broadcast(msg: String) extends Message
