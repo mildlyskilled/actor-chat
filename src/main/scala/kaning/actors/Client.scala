1270dfef8211199203952133b330ee0f585685df
Kwabena Aning <kwabena.aning@gmail.com>
2014-03-16 12:23:34 +0000

    Working with dynamic client IP address binding

--- src/main/scala/kaning/actors/Client.scala
+++ src/main/scala/kaning/actors/Client.scala
@@ -17,28 +17,59 @@ import scala.collection.JavaConversions._
 object ChatClientApplication {
 
   def main(args:Array[String]) {
-    println("Start Akka Chat Client Actor")
 
-     // construct client with current machine's IP address instead of using the config value
+    println("Starting Akka Chat Client Actor")
+    /* construct client with current machine's IP address instead of using the config value
+    * get network interfaces make sure it's not loopbak (i.e. points outside of itself)
+    * and filter for those that are actually "up"
+    */
     val interfaces = new JEnumerationWrapper(NetworkInterface.getNetworkInterfaces).toList.filter(!_.isLoopback).filter(_.isUp)
-    // Ideally this should give a list of
-    val ipAddress = interfaces.head.getInterfaceAddresses.filter(_.getBroadcast != null).head.getAddress.getHostAddress
-    val clientAddress = Address("akka.tcp", "AkkaChat", ipAddress, 2552)
-
-
+    /** Ideally this should give a list of ip addresses and then we choose the one we want
+    * but alas I am lazy so just pop the first ip address that works and use it instead
+    * I use getBroadcast here as a subtle way of filtering out IPV6 addresses they have 
+    * a null value
+    */
+    val ipAddress = interfaces.head.getInterfaceAddresses.filter(_.getBroadcast != null).head.getAddress.getHostAddress 
+    // In some circles this would be the username
     val identity = new ConsoleReader().readLine("identify yourself: ")
-    val system = ActorSystem("AkkaChat", ConfigFactory.load.getConfig("chatclient"))
-    val serverAddress = system.settings.config.getString("actor-chat.server.address")
-    val serverPort = system.settings.config.getString("actor-chat.server.port")
+
+    /* tinker with configurations so that our client uses it's own IP address and not one that is 
+    * hard-coded in application.conf. In short it this line was not there my ip would be 
+    * 127.0.0.1
+    */
+    val clientConfig = ConfigFactory.parseString(s"""akka.remote.netty.tcp.hostname="$ipAddress" """)
+    val defaultConfig = ConfigFactory.load.getConfig("chatclient")
+    val completeConfig = clientConfig.withFallback(defaultConfig)
+
+    /* construct system using the complete config which is a result of "merging"
+    * the parsed string and the default configs from the akka remote sub-system
+    */
+    val system = ActorSystem("AkkaChat", completeConfig)
+    
+    /*
+    * get the server reference here because we will bind and forward messages to
+    * it from our nifty console input
+    */
+    val serverconfig = ConfigFactory.load.getConfig("chatserver")
+    val serverAddress = serverconfig.getString("akka.remote.netty.tcp.hostname")
+    val serverPort = serverconfig.getString("akka.remote.netty.tcp.port")
     val serverPath = s"akka.tcp://AkkaChat@$serverAddress:$serverPort/user/chatserver"
+    val server = system.actorSelection(serverPath) // <-- this is where we get the server reference
+
+    // NOW CONSTRUCT THE CLIENT using as a member of the system defined above
+    val client = system.actorOf(Props(classOf[ChatClientActor]), name = identity)
+    
+    // some input parsing logic to filter out private messages and so special things to it
+    // like NOT Broadcast it to all connected clients
     val privateMessageRegex = """^@([^\s]+) (.*)$""".r
-    val server = system.actorSelection(serverPath)
-
-    val client = system.actorOf(Props(classOf[ChatClientActor], server, identity).withDeploy(Deploy(scope = RemoteScope(clientAddress))), name = identity)
 	
-	print("Client constructed: ")
-	println(client)
+    // we can implement a help feature here to explain the protocol
+    println("Type /join to join the chat room")
 
+    /* Iterate infinitely over a stream created from our jline console reader object and 
+    * use some functional concepts over this i.e. pattern matching takeWhile and the 
+    * lovely foreach
+    */
     Iterator.continually(new ConsoleReader().readLine("> ")).takeWhile(_ != "/exit").foreach { msg =>
       msg match {
         case "/list" =>
@@ -59,12 +90,13 @@ object ChatClientApplication {
     }
 
     println("Exiting...")
+    // Tell the server to remove us from currently connected clients
     server.tell(Unregister(identity), client)
-	exit()
+    // find a graceful way to exit the application here
   }
 }
 
-class ChatClientActor extends Actor {
+class ChatClientActor  extends Actor {
 
     def receive = {
 
